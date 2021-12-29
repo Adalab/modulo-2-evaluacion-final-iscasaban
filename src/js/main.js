36b351d79d240201e623b92f3ecbabe5b37bc25a
Inma Sánchez Casabán <iscasaban@gmail.com>
2021-12-29 11:27:25 +0100

    Added connection with API Jikan. Show list results with title&image

--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,32 @@
-'use strict';
+"use strict";
 
+// 1. Variables Globales: querySelector, datos que se van a usar en todas las funciones, de toda la página
+const animeTitle = document.querySelector(".js-input");
+const searchBtn = document.querySelector(".js-btn-search");
+const animeList = document.querySelector(".js-result");
+
+// 2. Funciones: se ordenan según gustos, pero se pueden agrupar por las que tengan que ver entre sí
+
+function getAnime(event) {
+  event.preventDefault();
+  const animeSearch = animeTitle.value;
+
+  fetch(`https://api.jikan.moe/v3/search/anime?q=${animeSearch}`)
+    .then((response) => response.json())
+    .then((data) => {
+      for (let index = 0; index < data.results.length; index++) {
+        const anime = data.results[index];
+        animeList.innerHTML += `<li><img src="${anime.image_url}" alt="${anime.title}" title="${anime.title}">  ${anime.title}</li>`;
+        console.log(animeList);
+      }
+    });
+}
+
+// function handleClickUpdate(event) {
+//   event.preventDefault();
+// }
+
+// 3. Código que se ejecuta cuando se carga la página: Listeners, pedir datos al servidor, leer datos de la memoria...
+
+// searchBtn.addEventListener("click", handleClickUpdate);
+searchBtn.addEventListener("click", getAnime);
