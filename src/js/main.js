30e6fa7e0c3afb02853db37c609954fba83f0238
Inma Sánchez Casabán <iscasaban@gmail.com>
2022-01-03 11:48:04 +0100

    Save or delete favourites in array when user clicks on search results

--- src/js/main.js
+++ src/js/main.js
@@ -5,10 +5,11 @@ const animeTitle = document.querySelector(".js-input");
 const searchBtn = document.querySelector(".js-btn-search");
 const animeList = document.querySelector(".js-result");
 const searchResetBtn = document.querySelector(".js-btn-reset");
-const favouritesIds = [];
+let animeFavourites = [];
 
 // 2. Funciones: se ordenan según gustos, pero se pueden agrupar por las que tengan que ver entre sí
 
+//función para hacer un fetch que devuelva resultados según la búsqueda de la usuaria:
 function getAnime(event) {
   event.preventDefault();
   const animeSearch = animeTitle.value;
@@ -21,35 +22,58 @@ function getAnime(event) {
     });
 }
 
+//función para pintar los resultados de la búsqueda (img y título del anime)
 function renderAnime(results) {
   for (let index = 0; index < results.length; index++) {
     const anime = results[index];
     const image = renderImg(anime.image_url, anime.title);
 
-    animeList.innerHTML += `<li data-animeid=${anime.mal_id} class="js-anime-item anime__card"> ${image} <h3>${anime.title}</h3></li>`;
+    animeList.innerHTML += `<li data-animeid='${anime.mal_id}' data-animetitle='${anime.title}' data-animeimage='${anime.image_url}' class="js-anime-item anime__card"> ${image} <h3>${anime.title}</h3></li>`;
   }
 
+  //añadimos un listener para saber en qué anime hace clic para añadir a favoritos
   const animeListElement = document.querySelectorAll(".js-anime-item");
   for (const animeElement of animeListElement) {
     animeElement.addEventListener("click", handleAnimeClick);
   }
 }
 
+// con esta función sabemos en qué elemento hace clic y lo añadimos a favoritos (toggle anime__fav--item) o lo eliminamos si ya estaba en el listado
 function handleAnimeClick(event) {
-  const cardElement = event.currentTarget;
-  const animeId = cardElement.dataset.animeid;
-  cardElement.classList.toggle("anime__fav--item");
-  const indexOfItem = favouritesIds.indexOf(animeId);
-  if (indexOfItem === -1) {
-    favouritesIds.push(animeId);
-  } else {
-    favouritesIds.splice(indexOfItem, 1);
-  } //así agregamos el id al array con cada clic
-  console.log(favouritesIds);
+  const clickedAnime = event.currentTarget;
+  clickedAnime.classList.toggle("anime__fav--item");
+
+  const animeId = clickedAnime.dataset.animeid;
+  const animeTitle = clickedAnime.dataset.animetitle;
+  const animeImg = clickedAnime.dataset.animeimage;
+  const animeFav = {
+    id: animeId,
+    title: animeTitle,
+    image: animeImg,
+  };
+
+  const findIndexAnime = animeFavourites.findIndex((item) => item.id === animeId);
+    if (findIndexAnime === -1) {//así comprobamos si el id ya está en favoritos. Si devuelve -1, no está.
+      animeFavourites.push(animeFav); //con push, lo añadimos al array de favoritos
+    } 
+    else {
+       animeFavourites.splice(findIndexAnime, 1); //si el id ya está en favoritos, no queremos que lo vuelva a agregar. Por eso usamos splice, que toma la posición con indexOf y borra ese elemento
+     }
+   console.log(animeFavourites);
+}
+
+//ahora queremos saber a qué título e img se corresponde cada id que tengo guardado en el array favouritesIds:
+
+function renderFavourites() {
+    //limpiar lista
+    for (const animeFav of animeFavourites) {
+        
+    //pintar cada favorito que hay en el array    
+    }
+     
 }
 
 //Para poder poner una imagen por defecto si no tiene, creo una función que me devuelva la imagen y que podré sustituir en el innerHTML y aplicarle lógica sin complicar el código del for
-
 function renderImg(imageUrl, altImage) {
   if (
     imageUrl ===
@@ -69,12 +93,7 @@ function clearSearch(event) {
   animeTitle.value = "";
 }
 
-// function handleClickUpdate(event) {
-//   event.preventDefault();
-// }
-
 // 3. Código que se ejecuta cuando se carga la página: Listeners, pedir datos al servidor, leer datos de la memoria...
 
-// searchBtn.addEventListener("click", handleClickUpdate);
 searchBtn.addEventListener("click", getAnime);
 searchResetBtn.addEventListener("click", clearSearch);
